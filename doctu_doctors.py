2733efe4c4546d26324f8db7f1e8105689117457
Guf-Hub <72359732+Guf-Hub@users.noreply.github.com>
2023-03-02 13:38:41 +0300

    first commit

--- doctu_doctors.py
+++ doctu_doctors.py
@@ -29,73 +29,113 @@ def get_doctors_info(link: str):
 
     try:
         block = soup.find('div', class_=re.compile('doc-name'))
-        name = block.find('h1').text.strip()
-        experience = block.find('div', class_='experience').text.strip().replace(u'\xa0', ' ').replace(u'\u202F', ' ')
-        city = block.find('div', class_='note').text.strip()
     except:
-        name = ''
-        city = ''
-        experience = ''
+        block = None
 
-    try:
-        specialty = soup.find('div', class_=re.compile('doc-name')).find('div', class_=re.compile(
-            'specialty')).text.strip()
-    except:
-        specialty = ''
+    if block:
+        try:
+            name = block.find('h1').text.strip()
+        except:
+            name = ''
 
-    try:
-        intro = soup.find('p', class_=re.compile('doc-intro')).text.strip().replace(u'\xa0', ' ').replace(u'\u202F',
-                                                                                                          ' ')
-    except:
-        intro = ''
+        try:
+            city = block.find('div', class_='note').text.strip()
+        except:
+            city = ''
 
-    try:
-        block1 = soup.find('div', class_=re.compile('col-xs-4 education'))
-        education = [b.getText().strip().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' - ') for b in
-                     block1.find_all('div', class_=re.compile("school$"))]
-        training = [b.getText().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' ').strip() for b in
-                    block1.find_all('div', class_='training')]
-        work = [b.getText().strip().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' - ') for b in
-                block1.find_all('div', class_='school work')]
-        education = [x for x in education if x not in work]
-    except:
-        education = []
-        work = []
-        training = []
+        try:
+            experience = block.find('div', class_='experience').text.strip().replace(u'\xa0', ' ').replace(u'\u202F',
+                                                                                                           ' ')
+        except:
+            experience = ''
 
-    try:
-        block2 = soup.find('div', class_=re.compile('col-xs-8 right_review'))
-        skills = block2.find('ul').text.replace('.', '').replace(u'\xa0', ' ').replace(u'\u202F', ' ').split(',')
-        address = [re.sub('\n+', '\n', ' * '.join(
-            b.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').strip().split('\n\n\n\n\n\n\n'))).split(' * ') for b in
-                   block2.find_all('section', class_='doc-address clearfix')][0]
-        address = address[0: len(address) - 1]
-        reviews = [re.sub('\n+', '\n', ' * '.join(
-            b.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').strip().split('\n\n\n\n\n\n\n'))).split(' * ') for b in
-                   block2.find_all('section', class_='reviews-list')][0]
-        reviews = re.sub('\n | \n', '\n', re.sub('г.\n\d+', 'г.', re.sub(' +', ' ',
-                                                                         ' * '.join(reviews).replace('\nПациент',
-                                                                                                     'Пациент')))).split(
-            ' * ')
-    except:
-        skills = []
-        address = []
-        reviews = []
+        try:
+            specialty = block.find('div', class_=re.compile('specialty')).text.strip()
+        except:
+            specialty = ''
 
-    data = {'link': link,
-            'name': name,
-            'experience': experience,
-            'city': city,
-            'specialty': specialty,
-            'intro': intro.replace(u'\xa0', ' ').replace(u'\u202F', ' '),
-            'education': education,
-            'training': training,
-            'work': work,
-            'skills': skills,
-            'address': address,
-            'reviews': reviews, }
+        try:
+            intro = soup.find('p', class_=re.compile('doc-intro')).text.strip().replace(u'\xa0', ' ').replace(u'\u202F',
+                                                                                                              ' ')
+        except:
+            intro = ''
 
-    return data
+        try:
+            block1 = soup.find('div', class_=re.compile('col-xs-4 education'))
+        except:
+            block1 = None
+
+        education = work = training = None
+        if block1:
+            try:
+                education = [b.getText().strip().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' - ') for
+                             b in
+                             block1.find_all('div', class_=re.compile("school$"))]
+                work = [b.getText().strip().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' - ') for b in
+                        block1.find_all('div', class_='school work')]
+                education = [x for x in education if x not in work]
+            except:
+                education = []
+                work = []
+
+            try:
+                training = [b.getText().replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace('\n', ' ').strip() for b
+                            in
+                            block1.find_all('div', class_='training')]
+            except:
+                training = []
+
+        try:
+            block2 = soup.find('div', class_=re.compile('col-xs-8 right_review'))
+        except:
+            block2 = None
+
+        skills = address = reviews = None
+        if block2:
+            try:
+                # skills = [f'{item}\n'.join(
+                #     [i.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace(';', '.').capitalize() for i in
+                #      block2.findAll('li')]) for item in block2.findAll('ul')]
+                skills = [i.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').replace(';', '.').capitalize() for i in block2.findAll('li')]
+            except:
+                skills = []
+
+            try:
+                address = [re.sub('\n+', '\n', ' * '.join(
+                    b.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').strip().split('\n\n\n\n\n\n\n'))).split(' * ')
+                           for
+                           b in block2.find_all('section', class_='doc-address clearfix')][0]
+                address = address[0: len(address) - 1]
+            except:
+                address = []
+
+            try:
+                reviews = [re.sub('\n+', '\n', ' * '.join(
+                    b.text.replace(u'\xa0', ' ').replace(u'\u202F', ' ').strip().split('\n\n\n\n\n\n\n'))).split(' * ')
+                           for
+                           b in block2.find_all('section', class_='reviews-list')][0]
+                reviews = re.sub('\n | \n', '\n', re.sub('г.\n\d+', 'г.', re.sub(' +', ' ',
+                                                                                 ' * '.join(reviews).replace(
+                                                                                     '\nПациент',
+                                                                                     'Пациент')))).split(
+                    ' * ')
+            except:
+                reviews = []
+
+        data = {'link': link,
+                'name': name,
+                'experience': experience,
+                'city': city,
+                'specialty': specialty,
+                'intro': intro,
+                'education': education,
+                'training': training,
+                'work': work,
+                'skills': skills,
+                'address': address,
+                'reviews': reviews, }
+
+        return data
 
 
 def multiprocess(link: str):
